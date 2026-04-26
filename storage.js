1af7194a5496a16764bf488f1e1753d2df3e9602
SachinthaGaurawa <gaurawasachintha@gmail.com>
2026-04-26 16:22:08 +0530

    Delete storage.js

--- storage.js
+++
@@ -1,5 +0,0 @@
-export const save = (key, value) =>
-  localStorage.setItem(key, JSON.stringify(value));
-
-export const load = (key, fallback) =>
-  JSON.parse(localStorage.getItem(key)) || fallback;
